7ddf7bab786ef3c499172845550cb103e3759f13
Stefan Zorbas <zorbastefan.coding@gmail.com>
2025-03-15 19:54:12 +0100

    first solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,90 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import UserPhoto from "./assets/images/image-jeremy.png";
+import EllipsisSvg from "./assets/images/icon-ellipsis.svg";
+
+const PERIODS = ["daily", "weekly", "monthly"];
 
 function App() {
-  return <></>;
+  const [period, setPeriod] = useState("weekly");
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log("Oops! Something went wrong.");
+        }
+      })
+      .then((data) => {
+        setActivities(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handlePeriod = (e) => {
+    setPeriod(e.target.value);
+  };
+
+  const periodsOutput = PERIODS.map((item) => (
+    <li key={item} className="periods__item">
+      <input
+        type="radio"
+        name="period"
+        id={item}
+        value={item}
+        onChange={handlePeriod}
+        checked={item === period}
+      />
+      <label htmlFor={item}>{item}</label>
+    </li>
+  ));
+
+  const activitiesOutput = activities.map((item) => {
+    const text = item.title.toLowerCase().replace(" ", "-");
+    return (
+      <li key={text} className={`dashboard__item dashboard__item--${text}`}>
+        <div>
+          <p>{item.title}</p>
+          <p>{item.timeframes[period].current}hrs</p>
+          <button type="button">
+            <img
+              src={EllipsisSvg}
+              alt="Small svg illustration of three dots."
+            />
+          </button>
+          <p>Last week - {item.timeframes[period].previous}hrs</p>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <main>
+      <section id="time-tracking-dashboard">
+        <ul className="dashboard__list">
+          <li className="dashboard__item dashboard__item--user">
+            <div>
+              <span>
+                <img src={UserPhoto} alt="An image of the user" />
+              </span>
+              <p>Report for</p>
+              <p>
+                <span>Jeremy</span>
+                <span>Robson</span>
+              </p>
+            </div>
+            <ul className="periods__list">{periodsOutput}</ul>
+          </li>
+          {activitiesOutput}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default App;
